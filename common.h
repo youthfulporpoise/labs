94362e37be151f3f4bf48bede56c01cc110c5328
H. Ali <>
2024-07-31 21:30:41 +0530

    A header file for common functions
    `common.h` reduces repetition by packaging common
    array procedures into routines.

---
+++ common.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Read an array size with a prompt which
+ * may be empty. */
+size_t read_size(char *msg)
+{
+    size_t x;
+    printf("%s", msg);
+    scanf("%zu", &x);
+    return x;
+}
+
+
+/* Read an array up to n elements. */
+void read_array(int *array, size_t n)
+{
+    printf("Enter %zu elements: ", n);
+    for (size_t i = 0; i < n; ++i)
+        scanf("%i", &array[i]);
+}
+
+
+/* Print an array up to n elements with a prompt
+ * which may be empty. */
+void print_array(char *msg, int *array, size_t n)
+{
+    printf("%s", msg);
+    for (size_t i = 0; i < n; ++i)
+        printf("%d ", array[i]);
+    puts("");
+}
