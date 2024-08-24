0f26cdf0d58f849bfb08c7c116f6b8a13840464d
H. Ali <>
2024-08-24 14:04:14 +0530

    `12_sparsematrix_repr.c` uses dynamic memory allocation.

--- 12_sparsematrix_repr.c
+++ 12_sparsematrix_repr.c
@@ -1,10 +1,17 @@
+/* SPARSE MATRIX REPRESENTATION
+ * 
+ * Represent a matrix with a large zero-to-non-zero elements
+ * (a sparse matrix) with memory efficiency.
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #define SM_COL 3
 #define SM_ROW 100
 
 int main ();
-void read_sm (char*, int(*)[SM_COL]);
-void print_sm (char*, int(*)[SM_COL]);
+int** read_sm(char*, int (*)[SM_COL]);
+void print_sm (char*, int (*)[SM_COL]);
 
 int main()
 {
@@ -12,21 +19,22 @@ int main()
     printf("Row size: "); scanf("%zu", &m);
     printf("Column size: "); scanf("%zu", &n);
 
-    int sm[SM_ROW][SM_COL];
+    int (*sm)[SM_COL] = malloc(sizeof (int[1][SM_COL]));
     sm[0][0] = m; sm[0][1] = n;
     printf("%d %d %d\n", sm[0][0], sm[0][1], sm[0][2]);
 
-    read_sm("Matrix: ", sm);
+    sm = (int(*)[SM_COL]) read_sm("Matrix: ", sm);
     print_sm("Sparse matrix representation: ", sm);
 
+    free(sm);
     return 0;
 }
 
-void read_sm(char *msg, int (*sm)[SM_COL])
+int** read_sm(char *msg, int (*sm)[SM_COL])
 {
     /* We assume that sm has non-zero size and that
      * sm contains the row and column size specified
-     * of the matrix. */
+     * of the matrix. sm will at least one row. */
     printf("%s\n", msg);
     size_t m = sm[0][0];
     size_t n = sm[0][1];
@@ -36,19 +44,21 @@ void read_sm(char *msg, int (*sm)[SM_COL])
         for (size_t j = 0; j < n; ++j) {
             scanf("%d", &v);
             if (v != 0) {
+                sm = realloc(sm, sizeof (int[k + 1][SM_COL]));
                 sm[k][0] = i;
                 sm[k][1] = j;
                 sm[k++][2] = v;
             }
         }
     }
-    sm[0][2] = k;
+    sm[0][2] = k - 1;
+    return (int**) sm;
 }
 
 void print_sm(char *msg, int (*sm)[SM_COL])
 {
     printf("%s\n", msg);
     size_t elemc = sm[0][2];
-    for (size_t k = 0; k < elemc; ++k)
+    for (size_t k = 0; k <= elemc; ++k)
         printf("%3d %3d %3d\n", sm[k][0], sm[k][1], sm[k][2]);
 }
