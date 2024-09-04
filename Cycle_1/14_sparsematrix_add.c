366b5bdb81c81b483eff88ac37e5614f936924e7
youthfulporpoise <hatalihim7d4@protonmail.com>
2024-09-04 20:25:15 +0530

    `14_sparsematrix_add.c`: complete

--- Cycle_1/14_sparsematrix_add.c
+++ Cycle_1/14_sparsematrix_add.c
@@ -15,14 +15,14 @@ int main()
 
     int (*sm1)[SM_COL] = malloc(sizeof (int[1][SM_COL]));
     sm1[0][0] = m; sm1[0][1] = n;
-    sm1 = (int(*)[SM_COL]) read_sm("Matrix #1:", sm1);
+    sm1 = (int(*)[SM_COL]) read_sm("\nMatrix #1:", sm1);
 
     int (*sm2)[SM_COL] = malloc(sizeof (int[1][SM_COL]));
     sm2[0][0] = m; sm2[0][1] = n;
-    sm2 = (int(*)[SM_COL]) read_sm("Matrix #2:", sm2);
+    sm2 = (int(*)[SM_COL]) read_sm("\nMatrix #2:", sm2);
 
-    print_sm("\n SM #1:", sm1);
-    print_sm("\n SM #2:", sm2);
+    print_sm("\nSM #1:", sm1);
+    print_sm("\nSM #2:", sm2);
 
     int (*sm3)[SM_COL] = (int(*)[SM_COL]) add_sm(sm1, sm2);
     print_sm("\nSum: ", sm3);
@@ -61,7 +61,6 @@ int** add_sm(int (*sm1)[SM_COL], int (*sm2)[SM_COL])
 
     size_t i = 1, j = 1, k = 1;
     while (i <= k1 && j <= k2) {
-        printf("%zu %zu %zu\n", k, i, j);
         sm3 = realloc(sm3, sizeof (int[k + 1][SM_COL]));
         int occur = occur_cmp(sm1[i][0], sm1[i][1], sm2[j][0], sm2[j][1]);
         if (occur == 0) {
@@ -75,20 +74,16 @@ int** add_sm(int (*sm1)[SM_COL], int (*sm2)[SM_COL])
             sm3[k++][2] = sm2[j++][2];
         }
     }
-    printf("%zu %zu %zu\n", k, i, j);
     while (i <= k1) {
-        printf("I: %zu %zu %zu\n", k, i, j);
+        sm3 = realloc(sm3, sizeof (int[k + 1][SM_COL]));
         sm3[k][0] = sm1[i][0]; sm3[k][1] = sm1[i][1];
         sm3[k++][2] = sm1[i++][2];
     }
     while (j <= k2) {
-        printf("J: %zu %zu %zu\n", k, i, j);
+        sm3 = realloc(sm3, sizeof (int[k + 1][SM_COL]));
         sm3[k][0] = sm2[j][0]; sm3[k][1] = sm2[j][1];
-        printf("JA: %d %d %d\n", sm2[j][0], sm2[j][1], sm2[j][2]);
         sm3[k++][2] = sm2[j++][2];
     }
-    printf("%zu %zu %zu\n", k, i, j);
-    sm3 = realloc(sm3, sizeof (int[k + 1][SM_COL]));
     sm3[0][2] = k - 1;
     return (int**) sm3;
 }
