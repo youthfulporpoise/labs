1be39883fce5262c4c254d9feb11a667e9953f0e
youthfulporpoise <hatalihim7d4@protonmail.com>
2024-09-15 20:40:21 +0530

    `12_sparsematrx_repr.c`: minor modifications

--- Cycle_1/12_sparsematrix_repr.c
+++ Cycle_1/12_sparsematrix_repr.c
@@ -1,17 +1,10 @@
-/* SPARSE MATRIX REPRESENTATION
- * 
- * Represent a matrix with a large zero-to-non-zero elements
- * (a sparse matrix) with memory efficiency.
- */
-
 #include <stdio.h>
-#include <stdlib.h>
 #define SM_COL 3
 #define SM_ROW 100
 
 int main ();
-int** read_sm(char*, int (*)[SM_COL]);
-void print_sm (char*, int (*)[SM_COL]);
+void read_sm (char*, int**);
+void print_sm (char*, int**);
 
 int main()
 {
@@ -19,46 +12,44 @@ int main()
     printf("Row size: "); scanf("%zu", &m);
     printf("Column size: "); scanf("%zu", &n);
 
-    int (*sm)[SM_COL] = malloc(sizeof (int[1][SM_COL]));
+    int sm[SM_COL][SM_ROW];
     sm[0][0] = m; sm[0][1] = n;
     printf("%d %d %d\n", sm[0][0], sm[0][1], sm[0][2]);
 
-    sm = (int(*)[SM_COL]) read_sm("Matrix: ", sm);
+    read_sm("Matrix: ", sm);
     print_sm("Sparse matrix representation: ", sm);
 
-    free(sm);
     return 0;
 }
 
-int** read_sm(char *msg, int (*sm)[SM_COL])
+void read_sm(char *msg, int **sm)
 {
     /* We assume that sm has non-zero size and that
      * sm contains the row and column size specified
-     * of the matrix. sm will at least one row. */
+     * of the matrix. */
     printf("%s\n", msg);
     size_t m = sm[0][0];
     size_t n = sm[0][1];
     size_t k = 1;
+    puts("Checkpoint 1");
     int v;
     for (size_t i = 0; i < m; ++i) {
         for (size_t j = 0; j < n; ++j) {
             scanf("%d", &v);
             if (v != 0) {
-                sm = realloc(sm, sizeof (int[k + 1][SM_COL]));
                 sm[k][0] = i;
                 sm[k][1] = j;
                 sm[k++][2] = v;
             }
         }
     }
-    sm[0][2] = k - 1;
-    return (int**) sm;
+    sm[0][2] = k;
 }
 
-void print_sm(char *msg, int (*sm)[SM_COL])
+void print_sm(char *msg, int **sm)
 {
     printf("%s\n", msg);
     size_t elemc = sm[0][2];
     for (size_t k = 0; k <= elemc; ++k)
-        printf("%3d %3d %3d\n", sm[k][0], sm[k][1], sm[k][2]);
+        printf("[%3d %3d %3d]\n", sm[k][0], sm[k][1], sm[k][2]);
 }
