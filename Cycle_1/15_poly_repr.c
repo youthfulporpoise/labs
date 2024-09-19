d5571448f2d866f973769eea5eff0bc582ab115e
youthfulporpoise <hatalihim7d4@protonmail.com>
2024-09-19 22:06:08 +0530

    `15_poly_repr.c`: a minor modification
    An insubstantial and non-manifesting (as regards this
    program) where the print procedure for a polynomial
    changes negative values to their corresponding positive
    values permanently.

--- Cycle_1/15_poly_repr.c
+++ Cycle_1/15_poly_repr.c
@@ -23,6 +23,7 @@ int main()
     Polynomial f;
     read_polynomial(&f);
     print_polynomial("Result: ", f);
+    free(f.p);
 }
 
 void read_polynomial(Polynomial* f) {
@@ -35,17 +36,19 @@ void read_polynomial(Polynomial* f) {
 
 void print_polynomial(char* msg, Polynomial f) {
     printf("%s", msg);
+    bool is_neg;
     for (size_t i = 0; i < f.n; ++i) {
+        is_neg = false;
         if (i != 0) {
             if (f.p[i].c < 0) {
                 printf(" - ");
-                f.p[i].c = -f.p[i].c;
+                is_neg = true;
             } else printf(" + ");
         }
 
         if (f.p[i].c == 0) continue;
         else if (f.p[i].c == 1);
-        else printf("%ld", f.p[i].c);
+        else printf("%ld", is_neg ? -f.p[i].c : f.p[i].c);
 
         if (f.p[i].e == 0);
         else if (f.p[i].e == 1) printf("x");
