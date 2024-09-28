7dcd039b8ace4144d50ec6e0b7f4e34fda1f85d3
H. Ali <>
2024-09-28 15:59:10 +0530

    Sensible naming 8 and 9 complete

---
+++ Cycle_2/8-CountFromFile/Main_30.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Main_30 {
+    public static void main(String[] args) {
+        try {
+            Scanner f = new Scanner(new File(args[0]));
+            int l = 0, w = 0, c = 0;
+            while (f.hasNextLine()) {
+                String line = f.nextLine();
+                l += 1;
+                c += line.length();
+                w += line.split(" ").length;
+            } System.out.printf("Lines: %d\nWords: %d\nCharacters: %d\n", l, w, c);
+            f.close();
+        } catch (FileNotFoundException e) {
+            System.err.printf("%s: does not exist\n", args[0]);
+            return;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("argument: must provide at least and at most one file");
+            return;
+        }
+    }
+}
---
+++ Cycle_2/9-CopyFile/Main_30.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class Main_30 {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader(new File(args[0]));
+            FileWriter fw = new FileWriter(new File(args[1]));
+            int x;
+            while ((x = fr.read()) != -1) fw.write(x);
+            fr.close();
+            fw.close();
+        } catch (FileNotFoundException e) {
+            System.err.printf("%s: does not exist\n", args[0]);
+            return;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Provide <source file> <destination file>");
+            return;
+        } catch (IOException e) {
+            System.out.printf("%s: cannot be opened\n", args[1]);
+            return;
+        }
+    }
+}
