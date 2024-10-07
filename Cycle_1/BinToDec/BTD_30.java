879477bf1a81390da9cfea98a2f073ab0486b8ac
H. Ali <>
2024-10-07 16:26:47 +0530

    Cycle 1: implementing decimal to binary

--- Cycle_1/BinToDec/BTD_30.java
+++ Cycle_1/BinToDec/BTD_30.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.lang.Math;
+import java.lang.StringBuffer;
 
 class BTD_30 {
     public static void main(String[] args) {
@@ -7,15 +7,27 @@ class BTD_30 {
         System.out.print("Binary number: ");
         int b = scanner.nextInt();
         System.out.println("Decimal number: " + binaryToDecimal(b));
+
+        System.out.print("\nDecimal number: ");
+        int d = scanner.nextInt();
+        System.out.println("Binary number: " + decimalToBinary(d));
     }
 
-    public static int binaryToDecimal(int b) {
+    public static String binaryToDecimal(int b) {
         int r = 0;
         int p = 0;
         while (b > 0) {
             r += b % 10 * Math.pow(2, p++);
             b = b / 10;
         }
-        return r;
+        return String.valueOf(r);
+    }
+
+    public static String decimalToBinary(int d) {
+        StringBuffer r = StringBuffer("");
+        while (d > 0) {
+            r.append(d % 2);
+            d /= 2;
+        } ;
     }
 }
