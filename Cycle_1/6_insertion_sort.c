7322aa90e0649ad05becfe9532a8b86d087fee3a
youthfulporpoise <hatalihim7d4@protonmail.com>
2024-10-10 18:24:40 +0530

    `6_insertion_sort.c`: corrected complexity and simplified implementation

--- Cycle_1/6_insertion_sort.c
+++ Cycle_1/6_insertion_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main ();
-void inssort (int*, size_t);
+void insertion_sort (int*, size_t);
 void shift_insert (int*, size_t);
 void read_array (char*, int*, size_t);
 void print_array (char*, int*, size_t);
@@ -12,28 +12,20 @@ int main()
     printf("Size: "); scanf("%zu", &n);
     int a[n];
     read_array("Array: ", a, n);
-    inssort(a, n);
+    insertion_sort(a, n);
     print_array("Result: ", a, n);
 }
 
-void inssort(int* a, size_t n)
+void insertion_sort(int* a, size_t n)
 {
     for (size_t i = 1; i < n; ++i) {
-        for (size_t j = 0; j < i; ++j) {
-            if (a[i] < a[j]) {
-                shift_insert(a + j, i - j + 1);
-                break;
-            }
-        }
+        size_t j = i;
+        char x = a[i];
+        while (j > 0 && x < a[j - 1]) {
+            a[j] = a[j - 1];
+            --j;
+        } a[j] = x;
     }
-}
-
-void shift_insert(int* a, size_t n)
-{
-    int v = a[n - 1];
-    for (size_t i = n - 1; i > 0; --i)
-        a[i] = a[i - 1];
-    a[0] = v;
 }
 
 void read_array(char* msg, int* a, size_t n)
