0948f655b21ef91423b70b2415cbbcb477e55a88
youthfulporpoise <hatalihim7d4@protonmail.com>
2024-11-06 19:16:52 +0530

    Find the second smallest number in an array: complete

--- Cycle_1/SecondSmallest/SecondSmallest_30.java
+++ Cycle_1/SecondSmallest/SecondSmallest_30.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class SecondSmallest_30 {
     public static void main (String[] args) {
-        
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Array size: "); int n = kb.nextInt();
+
+        int[] array = new int[n];
+        System.out.print("Array: ");
+        for (int i = 0; i < n; ++i)
+            array[i] = kb.nextInt();
+
+        /* We use a partial selection sort: O(n). */
+        int min;
+        for (int i = 0; i < 2; ++i) {
+            min = i;
+            for (int j = i + 1; j < n; ++j)
+                if (array[j] < array[min])
+                    min = j;
+            int tmp = array[i];
+            array[i] = array[min];
+            array[min] = tmp;
+        }
+        System.out.println("Second smallest: " + array[1]);
     }
 }
