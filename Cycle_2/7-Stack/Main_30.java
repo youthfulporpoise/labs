0cd749ae34fc069873ab44407ddfa4d36a2bbe2e
youthfulporpoise <hatalihim7d4@protonmail.com>
2024-11-07 23:14:11 +0530

    simplified stack and queue implementation

--- Cycle_2/7-Stack/Main_30.java
+++ Cycle_2/7-Stack/Main_30.java
@@ -1,55 +1,38 @@
+import java.util.Scanner;
+
 class Main_30 {
     public static void main(String[] args) {
-        Stack<Character> stack = new Stack<Character>(5);
+        Scanner kb = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<Integer>(5);
+        Queue<Integer> queue = new Queue<Integer>(5);
 
-        System.out.println("Pushing o, l, l, e, and h.");
-        char[] letters = {'o', 'l', 'l', 'e', 'h'};
+        int x;
         try {
-            for (char c : letters) stack.push(c);
-            System.out.println("Stack: " + stack);
-            System.out.println("Popping: " + stack.pop() + " popped.");
-        } catch (OverflowException e) {
-            System.out.println("Stack: OVERFLOW");
-        } catch (UnderflowException e) {
-            System.out.println("Stack: UNDERFLOW");
-        }
+            System.out.println("Stack and queue of size 5.");
+            System.out.print("Push elements: ");
+            for (x = 0; x < 5; ++x) stack.push(kb.nextInt());
+            System.out.print("Stack: " + stack + "\n");
+            System.out.print("Pop how many? ");
+            x = kb.nextInt();
+            for (; x > 0; --x) System.out.println("" + stack.pop());
 
-        // int choice;
-        // do {
-        //     System.out.print("\n" +
-        //             "(1) Push  (2) Pop (3) Display  (4) Quit\n" +
-        //             "Choice: ");
-        //     choice = kb.nextInt();
-        //     System.out.print("\n");
-        //     switch (choice) {
-        //         case 1:
-        //             System.out.print("Element: ");
-        //             try { stack.push(kb.next().charAt(0)); }
-        //             catch (OverflowException e) {
-        //                 System.out.println("\nThe stack is full.");
-        //                 continue;
-        //             } break;
-        //         case 2:
-        //             try { System.out.println("\nElement: " + stack.pop()); }
-        //             catch (UnderflowException e) {
-        //                 System.out.println("\nThe stack is empty.");
-        //                 continue;
-        //             } break;
-        //         case (3):
-        //             System.out.println("Stack: " + stack);
-        //             break;
-        //         case (-1):
-        //             return;
-        //         default:
-        //             System.out.println("\nI don‘t understand.");
-        //             continue;
-        //     }
-        // } while (choice != -1);
+            System.out.print("\nEnqueue elements: ");
+            for (x = 0; x < 5; ++x) queue.enqueue(kb.nextInt());
+            System.out.print("Queue: " + queue + "\n");
+            System.out.print("Dequeue how many? ");
+            x = kb.nextInt();
+            for (; x > 0; --x) System.out.println("" + queue.dequeue());
+        } catch (OverflowException e) {
+            System.out.println("Overflow");
+            return;
+        } catch (UnderflowException e) {
+            System.out.println("Underflow");
+            return;
+        }
     }
 }
 
 final class Stack<T> {
-    /** The Stack generic */
     private T[] stack;
     int size;
     private int top = 0;
@@ -76,6 +59,50 @@ final class Stack<T> {
         if (top > 0) return stack[--top];
         else throw new UnderflowException();
     }
+}
+
+final class Queue<T> {
+    private T[] queue;
+    int size;
+    private int f = 0, r = 0;
+
+    private enum QS { FULL, NEUTRAL, EMPTY };
+    private QS qs = QS.EMPTY;
+
+    public Queue(int n) {
+        this.size = n;
+        queue = (T[]) new Object[size];
+    }
+
+    @Override
+    public String toString() {
+        if (qs == QS.EMPTY) return "-/-";
+        else {
+            String repr = "";
+            int i = f;
+            do {
+                repr = repr.concat(queue[i] + " ");
+                i = (i + 1) % size;
+            } while (i != r);
+            return repr;
+        }
+    }
+
+    public void enqueue(T x) throws OverflowException {
+        if (qs == QS.FULL) throw new OverflowException();
+        queue[r] = x; r = (r + 1) % size;
+        if (r == f) qs = QS.FULL;
+        else qs = QS.NEUTRAL;
+    }
+
+    public T dequeue() throws UnderflowException {
+        if (qs == QS.EMPTY) throw new UnderflowException();
+        T x = queue[f]; f = (f + 1) % size;
+        if (r == f) qs = QS.EMPTY;
+        else qs = QS.NEUTRAL;
+        return x;
+    }
+
 }
 
 class UnderflowException extends Exception {
