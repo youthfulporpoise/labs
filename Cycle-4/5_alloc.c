38a7d269dba2eae09d0df9c69b29774f90cc1d28
youthfulporpoise <hatalihim7d4@protonmail.com>
2025-03-27 05:45:34 +0530

    `5_alloc`: code simplification

--- Cycle-4/5_alloc.c
+++ Cycle-4/5_alloc.c
@@ -38,12 +38,12 @@ void sort_dsc(int *a, size_t n)
     }
 }
 
-void first_fit(int *req, size_t n, int *part, size_t m)
+void first_fit(char *msg, int *req, size_t n, int *part, size_t m)
 {
     bool grant;
     int prev_size;
 
-    printf("\nFIRST FIT\n");
+    printf("\n%s\n", msg);
     printf("\n%12s %12s %12s %12s\n", "REQUEST", "PARTITION", "GRANT", "REMAINING");
     size_t i, j;
     for (i = 0; i < n; ++i) {
@@ -61,54 +61,28 @@ void first_fit(int *req, size_t n, int *part, size_t m)
     }
 }
 
-void best_fit(int *req, size_t n, int *part, size_t m)
+void best_fit(char *msg, int *req, size_t n, int *part, size_t m)
 {
+    for (size_t i = 0; i < m; ++i)
+        printf("%d ", part[i]);
+    puts("");
     sort_asc(part, m);
-
-    bool grant;
-    int prev_size;
-
-    printf("\nBEST FIT\n");
-    printf("\n%12s %12s %12s %12s\n", "REQUEST", "PARTITION", "GRANT", "REMAINING");
-    size_t i, j;
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < m; ++j) {
-            if (part[j] >= req[i]) grant = true;
-            else grant = false;
-
-            prev_size = part[j];
-            if (grant) {
-                part[j] -= req[i];
-                break;
-            } else continue;
-        }
-        printf("%12d %12d %12s %12d\n", req[i], prev_size, grant ? "Yes" : "No", j < m ? part[j] : prev_size);
-    }
+    for (size_t i = 0; i < m; ++i)
+        printf("%d ", part[i]);
+    puts("");
+    first_fit(msg, req, n, part, m);
 }
 
-void worst_fit(int *req, size_t n, int *part, size_t m)
+void worst_fit(char *msg, int *req, size_t n, int *part, size_t m)
 {
+    for (size_t i = 0; i < m; ++i)
+        printf("%d ", part[i]);
+    puts("");
     sort_dsc(part, m);
-
-    bool grant;
-    int prev_size;
-
-    printf("\nWORST FIT\n");
-    printf("\n%12s %12s %12s %12s\n", "REQUEST", "PARTITION", "GRANT", "REMAINING");
-    size_t i, j;
-    for (i = 0; i < n; ++i) {
-        for (j = 0; j < m; ++j) {
-            if (part[j] >= req[i]) grant = true;
-            else grant = false;
-
-            prev_size = part[j];
-            if (grant) {
-                part[j] -= req[i];
-                break;
-            } else continue;
-        }
-        printf("%12d %12d %12s %12d\n", req[i], prev_size, grant ? "Yes" : "No", j < m ? part[j] : prev_size);
-    }
+    for (size_t i = 0; i < m; ++i)
+        printf("%d ", part[i]);
+    puts("");
+    first_fit(msg, req, n, part, m);
 }
 
 
@@ -132,17 +106,13 @@ int main(void)
     for (size_t i = 0; i < n; ++i)
         scanf("%d", &req[i]);
 
-    int part_1[m];
-    memcpy(part_1, part, sizeof (int) * m);
-    first_fit(req, n, part_1, m);
+    int part_copy_1[m], part_copy_2[m];
+    memcpy(part_copy_1, part, sizeof (int) * m);
+    memcpy(part_copy_2, part, sizeof (int) * m);
 
-    int part_2[m];
-    memcpy(part_2, part, sizeof (int) * m);
-    best_fit(req, n, part_2, m);
-
-    int part_3[m];
-    memcpy(part_3, part, sizeof (int) * m);
-    worst_fit(req, n, part_3, m);
+    first_fit("FIRST FIT", req, n, part, m);
+    best_fit("BEST FIT", req, n, part_copy_1, m);
+    worst_fit("WORST FIT", req, n, part_copy_2, m);
 
     return 0;
 }
