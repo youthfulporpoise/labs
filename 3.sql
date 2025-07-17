b71ecb0570af836efdbfac1b41200e96fb145ccf
H. Ali <>
2025-07-17 15:18:35 +0530

    2, 3 complete

---
+++ 3.sql
@@ -0,0 +1,36 @@
+USE `23cs031`;
+
+-- CREATE TABLE employee (
+--     employee_name       VARCHAR(36),
+--     street              VARCHAR(24),
+--     city                VARCHAR(24)
+-- );
+-- 
+-- CREATE TABLE bank (
+--     bank_name           VARCHAR(36),
+--     city                VARCHAR(24)
+-- );
+-- 
+-- CREATE TABLE works (
+--     employee_name       VARCHAR(36),
+--     bank_name           VARCHAR(36),
+--     salary              FLOAT
+-- );
+-- 
+-- CREATE TABLE manages (
+--     employee_name       VARCHAR(36),
+--     manager_name        VARCHAR(36)
+-- );
+
+
+INSERT INTO employee VALUES
+    ("M. Harshvardhan",         "Breach Candy",         "Bombay"),
+    ("Mohammed Shah",           "Kamal Marg",           "Noida"),
+    ("Aisha Fathima",           "Edapally Jn",          "Edapally"),
+    ("G. H. Nair",              "Mangattu Rd",          "Ernakulam"),
+    ("Christina Jose",          "Theatre Rd",           "Kodungallur"),
+    ("Aliya Rahseed",           "Nethaji Rd",           "Pollachi"),
+    ("Selvi Ramakrishnan",      "Aruthra Nagar",        "Pondicherry"),
+    ("Sitara Jayakumar",        "English Church Rd",    "Palakkad")
+    ("Jennifer Thottathil",     "Double Cutting",       "Idukki"),
+    ("Aparna John",             "Anna Salai",           "Chennai");
