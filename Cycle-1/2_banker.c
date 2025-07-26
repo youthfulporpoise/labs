1411c6c2f786558e1426b825420bd8d562ffc4e6
youthfulporpoise <hatalihim7d4@protonmail.com>
2025-07-26 14:19:40 +0530

    `2_banker.c` rewritten

--- Cycle-1/2_banker.c
+++ Cycle-1/2_banker.c
@@ -2,85 +2,71 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    /* The resource count and process count resp. */
+    /*  The resource count and process count resp.  */
     size_t resc, prc;
     printf("Number of resources: ");
     scanf("%zu", &resc);
     printf("Number of processes: ");
     scanf("%zu", &prc);
 
-    unsigned allocated[prc][resc];
-    puts("\nALLOCATION >>>");
-    for (size_t i = 0; i < prc; ++i)
+    unsigned allocated[prc][resc],
+             maximum[prc][resc],
+             available[resc],
+             need[prc][resc];
+
+    puts("ALLOCATED =>");
+    for(size_t i = 0; i < prc; ++i)
         for (size_t j = 0; j < resc; ++j)
             scanf("%u", &allocated[i][j]);
 
-    unsigned maximum[prc][resc];
-    puts("\nMAXIMUM >>>");
-    for (size_t i = 0; i < prc; ++i)
+    puts("MAXIMUM =>");
+    for(size_t i = 0; i < prc; ++i)
         for (size_t j = 0; j < resc; ++j)
             scanf("%u", &maximum[i][j]);
 
-    unsigned available[resc];
-    puts("\nAVAILABLE >>>");
+    puts("AVAILABLE =>");
     for (size_t i = 0; i < resc; ++i)
         scanf("%u", &available[i]);
 
-    unsigned need[prc][resc];
-    for (size_t i = 0; i < prc; ++i)
-        for (size_t j = 0; j < resc; ++j)
+    puts("NEED =>");
+    for(size_t i = 0; i < prc; ++i) {
+        for (size_t j = 0; j < resc; ++j) {
             need[i][j] = maximum[i][j] - allocated[i][j];
-
-    puts("\nNEED");
-    for (size_t i = 0; i < prc; ++i) {
-        for (size_t j = 0; j < resc; ++j)
             printf("%u ", need[i][j]);
-        printf("\n");
+        } printf("\n");
     } printf("\n");
 
 
-    /* We simulate Banker’s algorithm to determine the safe state. */
-    bool safe;            /* Is a safe state achievable? (false is a placeholder) */
-    size_t safeseq[prc];  /* If so, what is the safe sequence? */
-    size_t sf = 0;        /* The safe sequence index */
-    bool finish[prc];     /* Which processes have succesfully terminated? */
+    /*  We simulate Banker’s algorithm to determine the safe state. */
+    bool safe;                  /*  Is a safe state achievable? (false is a placeholder)    */
+    size_t safeseq[prc];        /*  If so, what is the safe sequence?                       */
+    size_t sf = 0;              /*  The safe sequence index                                 */
+    bool finish[prc];           /*  Which processes have succesfully terminated?            */
+    bool suff = false;          /*  Are the available resources sufficient?                 */
+    size_t p = 0;               /*  The current process, also the current index             */
 
     for (size_t i = 0; i < prc; ++i)
         finish[i] = false;
 
-    bool suff = false;      /* Is the available resources sufficient? */
-    size_t p = 0;
     while (p < prc) {
-        /* If already terminated, skip. */
         if (finish[p]) {
             p++;
             continue;
         }
 
-        /* If the request can be granted, write safe sequence and terminate the
-         * process. */
         suff = true;
-        for (size_t i = 0; i < resc; ++i) {
-            if (need[p][i] > maximum[p][i]) {
-                suff = false;
-                break;
-            } else if (need[p][i] > available[i]) {
-                suff = false;
-                break;
-            }
-        }
-        if (suff) {
-            safeseq[sf++] = p;
-            finish[p] = true;
-        } else {
+        for (size_t i = 0; i < resc; ++i)
+            suff &= (need[p][i] <= maximum[p][i]) & (need[p][i] <= available[i]);
+
+        if (!suff) {
             p++;
             continue;
         }
-        
-        /* Release the acquired resources.  Enumerate the processes from the
-         * beginning afterwards. */
+
+        safeseq[sf++] = p;
+        finish[p] = true;
         for (size_t i = 0; i < resc; ++i) {
             available[i] += allocated[p][i];
             allocated[p][i] = 0;
