8144ea3ad20d2323a026f223677054739888d4d5
youthfulporpoise <hatalihim7d4@protonmail.com>
2025-09-02 13:05:02 +0530

    cycle-2 question-11 complete

---
+++ Cycle-2/11_sum.asm
@@ -0,0 +1,137 @@
+global        _start
+
+SECTION       .data
+  prmpt1      DB        "A = "
+  prmpt1_n    EQU       4
+  prmpt2      DB        "B = "
+  prmpt2_n    EQU       4
+  result      DB        "A + B = "
+  result_n    EQU       8
+  newl        DB        0xA
+
+SECTION       .bss
+  intgr       RESW      1
+  str1        RESB      32
+  str1_n      EQU       32
+  str2        RESB      32
+  str2_n      EQU       32
+  str3        RESB      32
+  str3_n      EQU       32
+
+SECTION       .text
+_start:
+  MOV         eax,      4
+  MOV         ebx,      1
+  MOV         ecx,      prmpt1
+  MOV         edx,      prmpt1_n
+  INT         0x80
+
+  MOV         eax,      3
+  MOV         ebx,      0
+  MOV         ecx,      str1
+  MOV         edx,      str1_n
+  INT         0x80
+  MOV         byte [str1 + eax - 1],  0x0
+
+  MOV         eax,      4
+  MOV         ebx,      1
+  MOV         ecx,      prmpt2
+  MOV         edx,      prmpt2_n
+  INT         0x80
+
+  MOV         eax,      3
+  MOV         ebx,      0
+  MOV         ecx,      str2
+  MOV         edx,      str2_n
+  INT         0x80
+  MOV         byte [str2 + eax - 1],  0x0
+
+  MOV         eax,      4
+  MOV         ebx,      1
+  MOV         ecx,      result
+  MOV         edx,      result_n
+  INT         0x80
+
+  MOV         esi,      str1
+  CALL        .atoi
+  MOV         [intgr],  eax
+  MOV         esi,      str2
+  CALL        .atoi
+
+  ADD         eax,      [intgr]
+  MOV         esi,      str3
+  CALL        .itoa
+
+  MOV         esi,      str3
+  CALL        .print_reverse
+
+  MOV         eax,      4
+  MOV         ebx,      1
+  MOV         ecx,      newl
+  MOV         edx,      1
+  INT         0x80
+
+  MOV         eax,      1
+  MOV         ebx,      0
+  INT         0x80
+
+; In:   esi -- null-appended string   (req.)
+; Out:  eax -- converted integer
+.atoi:
+  MOV       eax,        0
+
+.loop_atoi:
+  IMUL      eax,          10
+  MOV       bl,           byte [esi]
+  SUB       bl,           '0'
+  ADD       eax,          ebx
+
+  INC       esi
+  CMP       byte [esi],   0x0
+  JNE       .loop_atoi
+  RET
+
+; In:    ax -- integer                (req.)
+; Out:  esi -- destination string     (req.)
+.itoa:
+  MOV       bl,             10
+
+.loop_itoa:
+  XOR       ah,             ah
+  DIV       bl
+
+  ADD       ah,             '0'
+  MOV       [esi],          ah
+
+  INC       esi
+  CMP       al,             0
+  JNE       .loop_itoa
+  MOV       byte [esi],     0x0
+  RET
+
+; In:   esi -- string
+.print_reverse:
+  PUSH      0x0
+
+.push_loop:
+  CMP       byte [esi],     0x0
+  JE        .pop_loop
+  PUSH      esi
+  INC       esi
+  JMP       .push_loop
+
+.pop_loop:
+  POP       esi
+  CMP       esi,            0x0
+  JE        .print_reverse_ret
+    
+  MOV       eax,            4
+  MOV       ebx,            1
+  MOV       ecx,            esi
+  MOV       edx,            1
+  INT       0x80
+
+  JMP       .pop_loop
+
+.print_reverse_ret:
+  RET
