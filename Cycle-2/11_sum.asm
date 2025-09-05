a461f3168e1886d4b1878dea35c40de46bbb582f
youthfulporpoise <hatalihim7d4@protonmail.com>
2025-09-05 11:33:47 +0530

    added difference function in `11_sum.asm`

--- Cycle-2/11_sum.asm
+++ Cycle-2/11_sum.asm
@@ -5,18 +5,21 @@ SECTION       .data
   prmpt1_n    EQU       4
   prmpt2      DB        "B = "
   prmpt2_n    EQU       4
-  result      DB        "A + B = "
-  result_n    EQU       8
+  result1     DB        "A + B = "
+  result1_n   EQU       8
+  result2     DB        "A - B = "
+  result2_n   EQU       8
   newl        DB        0xA
 
 SECTION       .bss
-  intgr       RESW      1
+  sum         RESB      32
+  diff        RESW      32
+  intgr1      RESW      1
+  intgr2      RESW      1
   str1        RESB      32
   str1_n      EQU       32
   str2        RESB      32
   str2_n      EQU       32
-  str3        RESB      32
-  str3_n      EQU       32
 
 SECTION       .text
 _start:
@@ -46,30 +49,39 @@ _start:
   INT         0x80
   MOV         byte [str2 + eax - 1],  0x0
 
-  MOV         eax,      4
-  MOV         ebx,      1
-  MOV         ecx,      result
-  MOV         edx,      result_n
-  INT         0x80
-
   MOV         esi,      str1
   CALL        .atoi
-  MOV         [intgr],  eax
+  MOV         [intgr1], eax
+
   MOV         esi,      str2
   CALL        .atoi
+  MOV         [intgr2], eax
 
-  ADD         eax,      [intgr]
-  MOV         esi,      str3
+  MOV         eax,      4
+  MOV         ebx,      1
+  MOV         ecx,      result1
+  MOV         edx,      result1_n
+  INT         0x80
+
+  MOV         eax,      [intgr1]
+  ADD         eax,      [intgr2]
+  MOV         esi,      sum
   CALL        .itoa
-
-  MOV         esi,      str3
+  MOV         esi,      sum
   CALL        .print_reverse
 
   MOV         eax,      4
   MOV         ebx,      1
-  MOV         ecx,      newl
-  MOV         edx,      1
+  MOV         ecx,      result2
+  MOV         edx,      result2_n
   INT         0x80
+
+  MOV         eax,      [intgr1]
+  SUB         eax,      [intgr2]
+  MOV         esi,      diff
+  CALL        .itoa
+  MOV         esi,      diff
+  CALL        .print_reverse
 
   MOV         eax,      1
   MOV         ebx,      0
@@ -134,4 +146,10 @@ _start:
   JMP       .pop_loop
 
 .print_reverse_ret:
+  MOV         eax,      4
+  MOV         ebx,      1
+  MOV         ecx,      newl
+  MOV         edx,      1
+  INT         0x80
+
   RET
